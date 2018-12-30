28ef3797f599fde5815ee96168973aaf63533f79
mybichu <yuxiuyan123@qq.com>
2018-12-30 15:47:54 +0800

    add SolarSystem project files

---
+++ Assets/scripts/RotateSelf.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateSelf : MonoBehaviour {
+    public float angle = 2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(this.transform.position, Vector3.up, angle);  //行星自转
+	}
+}
